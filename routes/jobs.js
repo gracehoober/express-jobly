5c292d9cc07af84dcfd58da9083a00eb6ceadae2
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-09-22 11:26:55 -0700

    job schemas created and  job routes created

---
+++ routes/jobs.js
@@ -0,0 +1,125 @@
+"use strict";
+
+/** Routes for companies. */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+
+const { BadRequestError } = require("../expressError");
+const { ensureLoggedIn, ensureIsAdmin } = require("../middleware/auth");
+const Job = require("../models/job");
+
+const jobNewSchema = require("../schemas/jobNew.json");
+const jobUpdateSchema = require("../schemas/jobUpdate.json");
+const jobFilterSchema = require("../schemas/jobFilter.json");
+
+const prepareData = require("../helpers/utils");
+const router = new express.Router();
+
+
+/** POST / { company } =>  { company }
+ *
+ * company should be { handle, name, description, numEmployees, logoUrl }
+ *
+ * Returns { handle, name, description, numEmployees, logoUrl }
+ *
+ * Authorization required: admin
+ */
+
+router.post("/", ensureIsAdmin, async function (req, res, next) {
+  const validator = jsonschema.validate(
+    req.body,
+    jobNewSchema,
+    { required: true }
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const job = await Job.create(req.body);
+  return res.status(201).json({ job });
+});
+
+/** GET /  =>
+ *   { companies: [ { handle, name, description, numEmployees, logoUrl }, ...] }
+ *
+ * Can filter on provided search filters:
+ * - minEmployees
+ * - maxEmployees
+ * - nameLike (will find case-insensitive, partial matches)
+ *
+ * Authorization required: none
+ */
+
+router.get("/", async function (req, res, next) {
+
+  const data = prepareData(req.query);
+
+  const validator = jsonschema.validate(
+    data,
+    jobFilterSchema,
+    { required: true }
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const jobs = await Job.findAll(data);
+  return res.json({ jobs });
+});
+
+/** GET /[handle]  =>  { company }
+ *
+ *  Company is { handle, name, description, numEmployees, logoUrl, jobs }
+ *   where jobs is [{ id, title, salary, equity }, ...]
+ *
+ * Authorization required: none
+ */
+
+router.get("/:handle", async function (req, res, next) {
+  const job = await Job.get(req.params.handle);
+
+  return res.json({ job });
+});
+
+/** PATCH /[handle] { fld1, fld2, ... } => { company }
+ *
+ * Patches company data.
+ *
+ * fields can be: { name, description, numEmployees, logo_url }
+ *
+ * Returns { handle, name, description, numEmployees, logo_url }
+ *
+ * Authorization required: admin
+ */
+
+router.patch("/:handle", ensureIsAdmin, async function (req, res, next) {
+
+  const validator = jsonschema.validate(
+    req.body,
+    jobUpdateSchema,
+    { required: true }
+  );
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const job = await Job.update(req.params.handle, req.body);
+  return res.json({ job });
+});
+
+/** DELETE /[handle]  =>  { deleted: handle }
+ *
+ * Authorization: admin
+ */
+
+router.delete("/:handle", ensureIsAdmin, async function (req, res, next) {
+  await Job.remove(req.params.handle);
+  return res.json({ deleted: req.params.handle });
+});
+
+
+module.exports = router;
