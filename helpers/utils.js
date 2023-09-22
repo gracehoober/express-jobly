b9edcec64a7a5e4850f70637b4a55f47cc0a0f41
Joshua <jhellst@gmail.com>
2023-09-22 11:57:03 -0700

    Deployed to render and completed job filter

--- helpers/utils.js
+++ helpers/utils.js
@@ -13,7 +13,7 @@ function prepareData(data) {
 
   if (data.minSalary) data.minSalary = Number(data.minSalary);
   if (data.hasEquity === "true") data.hasEquity = true;
-  if (data.hasEquity === "false") data.hasEquity = false;
+  if (data.hasEquity === "false") data.hasEquity = 0;
 
   if ((data.minEmployees && data.maxEmployees) && (data.minEmployees > data.maxEmployees)) {
     throw new BadRequestError("Minimum employees must be less than maximum employees");
